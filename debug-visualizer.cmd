c6d3eb507fad4fb2d4b8588fb3de9a3b22d7ecda
Russell Klenk <contact@russellklenk.com>
2016-03-19 16:58:26 -0700

    Add script to launch Visual Studio and debug the profile visualizer.

---
+++ debug-visualizer.cmd
@@ -0,0 +1,12 @@
+@ECHO OFF
+
+SET VSVERSION_2013=12.0
+SET VSVERSION_2015=14.0
+SET VSVERSION=%VSVERSION_2013%
+
+IF NOT DEFINED DevEnvDir (
+    CALL "C:\Program Files (x86)\Microsoft Visual Studio %VSVERSION%\VC\vcvarsall.bat" x86_amd64
+)
+
+start devenv /debugexe build\profviz.exe
+
