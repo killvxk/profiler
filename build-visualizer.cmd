cb1557c4cc28cd898b8086d01c6d6cbfe0d60f26
Russell Klenk <contact@russellklenk.com>
2016-03-20 08:25:45 -0700

    Link to tdh.lib (event parsing helper lib).

--- build-visualizer.cmd
+++ build-visualizer.cmd
@@ -12,7 +12,7 @@ SET OUTPUTDIR="%~dp0build"
 SET INCLUDES=-I..\include -I..\manifest -I..\src
 SET DEFINES=/D _WIN32_WINNT=0x06000 /D UNICODE /D _UNICODE /D BUILD_STATIC /D _STDC_FORMAT_MACROS
 SET CPPFLAGS=%INCLUDES% /FC /nologo /W4 /WX /wd4505 /Zi /EHsc /Ob2it
-SET LIBRARIES=User32.lib Gdi32.lib Shell32.lib Advapi32.lib winmm.lib
+SET LIBRARIES=User32.lib Gdi32.lib Shell32.lib Advapi32.lib winmm.lib tdh.lib
 SET LNKFLAGS=%LIBRARIES%
 
 IF NOT EXIST %OUTPUTDIR% mkdir %OUTPUTDIR%
