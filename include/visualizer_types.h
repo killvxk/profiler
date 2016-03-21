101f5ea1cad316ac83349381f13e74d076fa90a9
Russell Klenk <contact@russellklenk.com>
2016-03-20 20:26:43 -0700

    Added additional data types for processes, threads and images.

--- include/visualizer_types.h
+++ include/visualizer_types.h
@@ -23,6 +23,56 @@ struct WIN32_CONTEXT_SWITCH
     int8_t     CurrThreadPriority; /// The priority value of the thread being switched in.
 };
 
+/// @summary Defines the data associated with the lifetime of an image, process or thread.
+struct WIN32_LIFETIME
+{
+    uint64_t   CreateTime;         /// The timestamp value (in ticks) at which the process or thread was created, or the image was loaded.
+    uint64_t   DestroyTime;        /// The timestamp value (in ticks) at which the process or thread was destroyed, or the image was unloaded.
+};
+
+/// @summary Defines the 
+struct WIN32_IMAGE_INFO
+{
+    WCHAR                              *ImagePath;         /// A zero-terminated string specifying the path of the image file.
+};
+
+/// @summary Defines the data associated with each thread that existed at some point during a process lifetime.
+struct WIN32_THREAD_INFO
+{
+    uint32_t                            ThreadId;          /// The operating system thread identifier.
+    uint32_t                            EntryAddress;      /// The address of the thread entry point relative to the image base address.
+    WCHAR                              *EntryPointName;    /// A zero-terminated string corresponding to the entry point symbol.
+};
+
+/// @summary Defines the data associated with each process that existed at some point during the trace capture.
+struct WIN32_PROCESS_INFO
+{
+    uint32_t                            ProcessId;         /// The operating system process identifier.
+    uint32_t                            Reserved;          /// Reserved for future use. Set to 0.
+    WCHAR                              *Executable;        /// The path of the process executable image.
+    WIN32_IMAGE_INFO                   *ProcessImage;      /// Points to additional information about the process executable image.
+    std::vector<uint32_t>               ThreadId;          /// The operating system identifier for each thread that existed at some point during the process lifetime.
+    std::vector<WIN32_LIFETIME>         ThreadLifetime;    /// The creation and destruction time for each thread that existed at some point during the process lifetime.
+    std::vector<WIN32_THREAD_INFO>      ThreadInfo;        /// Additional information about each thread that existed at some point during the process lifetime.
+    std::vector<uint32_t>               ImageBaseAddress;  /// The base load address for each image that existed at some point during the process lifetime.
+    std::vector<uint32_t>               ImagePathHash;     /// The 32-bit hash of the file path for each image that existed at some point during the process lifetime.
+    std::vector<WIN32_LIFETIME>         ImageLifetime;     /// The load and unload time for each image that existed at some point during the process lifetime.
+    std::vector<WIN32_IMAGE_INFO>       ImageInfo;         /// Additional information about each image that existed at some point during the process lifetime.
+    std::vector<uint64_t>               ContextSwitchTime; /// The timestamp (in ticks) at which each context switch occurred.
+    std::vector<WIN32_CONTEXT_SWITCH>   SwitchInData;      /// TODO(rlk): Is this the right way to keep this information?
+    std::vector<WIN32_CONTEXT_SWITCH>   SwitchOutData;     /// TODO(rlk): Is this the right way to keep this information?
+};
+
+/// @summary Defines the data associated with the list of processes that have produced events in the trace.
+struct WIN32_PROCESS_LIST
+{
+    size_t                              ProcessCount;      /// The number of processes defined in the process list.
+    std::vector<uint32_t>               ProcessId;         /// The operating system identifier for each process in the list.
+    std::vector<uint32_t>               ProcessHash;       /// The 32-bit hash value of the image path for each process.
+    std::vector<WIN32_LIFETIME>         ProcessLifetime;   /// The creation and destruction time for each process in the list.
+    std::vector<WIN32_PROCESS_INFO>     ProcessInfo;       /// Additional information about each process in the list.
+};
+
 /// @summary Define the data used to locate a task definition at a specific time.
 struct WIN32_TASK_AND_TIME
 {
