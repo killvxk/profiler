f4e470dbb0d896807d90c6353f2efd3b53f12e79
Russell Klenk <contact@russellklenk.com>
2016-04-18 06:27:32 -0700

    Added support for visualizer debug configuration.

--- build-visualizer.cmd
+++ build-visualizer.cmd
@@ -8,17 +8,41 @@ IF NOT DEFINED DevEnvDir (
     CALL "C:\Program Files (x86)\Microsoft Visual Studio %VSVERSION%\VC\vcvarsall.bat" x86_amd64
 )
 
-SET LIBDIR="%~dp0lib"
 SET OUTPUTDIR="%~dp0build"
-SET INCLUDES=-I..\include -I..\manifest -I..\src -I..\lib\%VSVERSION%
-SET DEFINES=/D _WIN32_WINNT=0x06000 /D UNICODE /D _UNICODE /D BUILD_STATIC /D _STDC_FORMAT_MACROS
-SET CPPFLAGS=%INCLUDES% /FC /nologo /W4 /WX /wd4505 /Zi /EHsc /Ob2it
-SET LIBRARIES=User32.lib Gdi32.lib Shell32.lib Advapi32.lib winmm.lib tdh.lib %LIBDIR%\%VSVERSION%\glfw3.lib
+SET INCLUDES=-I..\include -I..\manifest -I..\src
+SET LIBRARIES=User32.lib Gdi32.lib Shell32.lib Advapi32.lib winmm.lib tdh.lib glfw3.lib
+
+SET DEFINES_COMMON=/D _WIN32_WINNT=0x0600 /D UNICODE /D _UNICODE /D _STDC_FORMAT_MACROS
+SET DEFINES_DEBUG=%DEFINES_COMMON% /D DEBUG /D _DEBUG
+SET DEFINES_RELEASE=%DEFINES_COMMON% /D NDEBUG /D _NDEBUG
+
+SET CPPFLAGS_COMMON=%INCLUDES% /FC /nologo /W4 /WX /wd4505 /Zi /EHsc
+SET CPPFLAGS_DEBUG=%CPPFLAGS_COMMON% /Od
+SET CPPFLAGS_RELEASE=%CPPFLAGS_COMMON% /Ob2it
+
 SET LNKFLAGS=%LIBRARIES%
 
 IF NOT EXIST %OUTPUTDIR% mkdir %OUTPUTDIR%
 
+IF [%1] NEQ [] (
+    IF "%1" == "debug" (
+        SET DEFINES=%DEFINES_DEBUG%
+        SET CPPFLAGS=%CPPFLAGS_DEBUG%
+        @ECHO Building debug configuration...
+    ) ELSE (
+        SET DEFINES=%DEFINES_RELEASE%
+        SET CPPFLAGS=%CPPFLAGS_RELEASE%
+        @ECHO Building release configuration - did you mean to specify "debug" instead of "%1"?
+    )
+) ELSE (
+    SET DEFINES=%DEFINES_RELEASE%
+    SET CPPFLAGS=%CPPFLAGS_RELEASE%
+    @ECHO Building release configuration...
+)
+
 PUSHD %OUTPUTDIR%
-cl %CPPFLAGS% ..\src\visualizer.cc %DEFINES% %LNKFLAGS% /Feprofviz.exe
+cl %CPPFLAGS% ..\src\visualizer.cc %DEFINES% %LNKFLAGS% /Feprofviz.exe /link /LIBPATH:..\lib\%VSVERSION%
 POPD
+
+@ECHO Build complete.
 
